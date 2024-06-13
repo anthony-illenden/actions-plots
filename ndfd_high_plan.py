13359464a408dce54308002813ddc4237f82e42a
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-06-13 19:51:47 -0400

    Update ndfd_high_plan.py

--- ndfd_high_plan.py
+++ ndfd_high_plan.py
@@ -14,20 +14,15 @@ ds = xr.open_dataset(url, engine='netcdf4')
 ds = ds.metpy.parse_cf()
 ds_latlon = ds.metpy.assign_latitude_longitude()
 
-time_dim = None
-for dim in ds_latlon['Maximum_temperature_height_above_ground_12_Hour_Maximum'].dims:
-    if dim.startswith('time'):
+for dim in ['time', 'time1', 'time2', 'time3']:
+    if dim in hi_temp.dims:
         time_dim = dim
         break
-if time_dim:
-    hi_temp = ds_latlon['Maximum_temperature_height_above_ground_12_Hour_Maximum'].sel(**{time_dim: slice(start_date, None), 'height_above_ground1': 2})
-else:
-    print("No time dimension found")
 
 for i in range(0, 9):
-    datetime = formatted_date = np.datetime_as_string(hi_temp.time1[i].values, unit='D')
+    datetime = np.datetime_as_string(hi_temp[time_dim].values[i], unit='D')
     temp_f = (hi_temp[i,:,:] - 273.15) * 9/5 + 32
-    fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+    fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': ccrs.PlateCarree()})
     ax.set_extent([-86, -82, 41, 45])
     ax.add_feature(cfeature.STATES, linewidth=0.5)
     ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
@@ -36,7 +31,7 @@ for i in range(0, 9):
     cbar.set_ticks(np.arange(30, 111, 10))
     cbar.set_ticklabels(['30', '40', '50', '60', '70', '80', '90', '100', '110'])
     contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, colors='black', levels=np.arange(30, 111, 5))
-    plt.clabel(contour, inline=True, fontsize=12, fmt='%1.0f')
+    plt.clabel(contour, inline=True, fontsize=8, fmt='%1.0f')
     plt.title('NDFD High Temperature for {}'.format(datetime))
     plt.savefig('plots/high/NDFD_High_Temp_{}.png'.format(i), dpi=450, bbox_inches='tight')
 
