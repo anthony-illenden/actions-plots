adf14012402b87bba2c3c852f17860f7181e0418
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-06-13 20:09:54 -0400

    Update ndfd_high_plan.py

--- ndfd_high_plan.py
+++ ndfd_high_plan.py
@@ -14,24 +14,35 @@ ds = xr.open_dataset(url, engine='netcdf4')
 ds = ds.metpy.parse_cf()
 ds_latlon = ds.metpy.assign_latitude_longitude()
 
-for dim in ['time', 'time1', 'time2', 'time3']:
+hi_temp = ds_latlon['Maximum_temperature_height_above_ground_12_Hour_Maximum'].sel(height_above_ground1=2)
+
+possible_time_dims = ['time', 'time1', 'time2', 'time3']
+
+time_dim = None
+for dim in possible_time_dims:
     if dim in hi_temp.dims:
         time_dim = dim
         break
+if time_dim is None:
+    print("No valid time dimension found in hi_temp.")
 
-for i in range(0, 9):
-    datetime = np.datetime_as_string(hi_temp[time_dim].values[i], unit='D')
-    temp_f = (hi_temp[i,:,:] - 273.15) * 9/5 + 32
-    fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': ccrs.PlateCarree()})
-    ax.set_extent([-86, -82, 41, 45])
-    ax.add_feature(cfeature.STATES, linewidth=0.5)
-    ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
-    plt.contourf(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, cmap='jet', levels=np.arange(30, 111, 1))
-    cbar = plt.colorbar(label='Temperature (F)', fraction=0.046, pad=0.04)
-    cbar.set_ticks(np.arange(30, 111, 10))
-    cbar.set_ticklabels(['30', '40', '50', '60', '70', '80', '90', '100', '110'])
-    contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, colors='black', levels=np.arange(30, 111, 5))
-    plt.clabel(contour, inline=True, fontsize=8, fmt='%1.0f')
-    plt.title('NDFD High Temperature for {}'.format(datetime))
-    plt.savefig('plots/high/NDFD_High_Temp_{}.png'.format(i), dpi=450, bbox_inches='tight')
+start_date_formatted = np.datetime64(start_date)
 
+hi_temp = hi_temp.sel(**{time_dim: slice(start_date_formatted, None)})
+
+if time_dim:
+    for i in range(min(9, len(hi_temp[time_dim]))):  # Limit to the length of available data
+        datetime_str = np.datetime_as_string(hi_temp[time_dim].values[i], unit='D')
+        temp_f = (hi_temp[i,:,:] - 273.15) * 9/5 + 32
+        fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+        ax.set_extent([-86, -82, 41, 45])
+        ax.add_feature(cfeature.STATES, linewidth=0.5)
+        ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
+        plt.contourf(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, cmap='jet', levels=np.arange(30, 111, 1))
+        cbar = plt.colorbar(label='Temperature (F)', fraction=0.046, pad=0.04)
+        cbar.set_ticks(np.arange(30, 111, 10))
+        cbar.set_ticklabels(['30', '40', '50', '60', '70', '80', '90', '100', '110'])
+        contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, colors='black', levels=np.arange(30, 111, 5))
+        plt.clabel(contour, inline=True, fontsize=8, fmt='%1.0f')
+        plt.title('NDFD High Temperature for {}'.format(datetime_str))
+        plt.savefig('plots/high/NDFD_High_Temp_{}.png'.format(i), dpi=450, bbox_inches='tight')
