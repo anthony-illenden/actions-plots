66536eed9d957d6481d25d4348389a04c4fbddf4
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-06-13 20:12:27 -0400

    Update ndfd_low_plan.py

--- ndfd_low_plan.py
+++ ndfd_low_plan.py
@@ -14,30 +14,37 @@ ds = xr.open_dataset(url, engine='netcdf4')
 ds = ds.metpy.parse_cf()
 ds_latlon = ds.metpy.assign_latitude_longitude()
 
+low_temp = ds_latlon['Minimum_temperature_height_above_ground_12_Hour_Minimum'].sel(height_above_ground1=2)
+
+possible_time_dims = ['time', 'time1', 'time2', 'time3']
+
 time_dim = None
-for dim in ds_latlon['Minimum_temperature_height_above_ground_12_Hour_Minimum'].dims:
-    if dim.startswith('time'):
+for dim in possible_time_dims:
+    if dim in low_temp.dims:
         time_dim = dim
         break
+if time_dim is None:
+    print("No valid time dimension found in hi_temp.")
+
+start_date_formatted = np.datetime64(start_date)
+
+low_temp = low_temp.sel(**{time_dim: slice(start_date_formatted, None)})
+
 if time_dim:
-    low_temp = ds_latlon['Minimum_temperature_height_above_ground_12_Hour_Minimum'.sel(**{time_dim: slice(start_date, None), 'height_above_ground1': 2})
-else:
-    print("No time dimension found")
-    
-for i in range(0, 8):
-    datetime = formatted_date = np.datetime_as_string(low_temp.time2[i].values, unit='D')
-    temp_f = (low_temp[i,:,:] - 273.15) * 9/5 + 32
-    fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
-    ax.set_extent([-86, -82, 41, 45])
-    ax.add_feature(cfeature.STATES, linewidth=0.5)
-    ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
-    plt.contourf(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, cmap='jet', levels=np.arange(30, 111, 1))
-    cbar = plt.colorbar(label='Temperature (F)', fraction=0.046, pad=0.04)
-    cbar.set_ticks(np.arange(30, 111, 10))
-    cbar.set_ticklabels(['30', '40', '50', '60', '70', '80', '90', '100', '110'])
-    contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, colors='black', levels=np.arange(30, 111, 5))
-    plt.clabel(contour, inline=True, fontsize=8, fmt='%1.0f')
-    plt.title('NDFD Low Temperature for {}'.format(datetime))
-    plt.savefig('plots/low/NDFD_Low_Temp_{}.png'.format(i), dpi=450, bbox_inches='tight')
+    for i in range(min(9, len(low_temp[time_dim]))):  
+        datetime_str = np.datetime_as_string(low_temp[time_dim].values[i], unit='D')
+        temp_f = (hi_temp[i,:,:] - 273.15) * 9/5 + 32
+        fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+        ax.set_extent([-86, -82, 41, 45])
+        ax.add_feature(cfeature.STATES, linewidth=0.5)
+        ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
+        plt.contourf(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, cmap='jet', levels=np.arange(30, 111, 1))
+        cbar = plt.colorbar(label='Temperature (F)', fraction=0.046, pad=0.04)
+        cbar.set_ticks(np.arange(30, 111, 10))
+        cbar.set_ticklabels(['30', '40', '50', '60', '70', '80', '90', '100', '110'])
+        contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, colors='black', levels=np.arange(30, 111, 5))
+        plt.clabel(contour, inline=True, fontsize=8, fmt='%1.0f')
+        plt.title('NDFD Low Temperature for {}'.format(datetime))
+        plt.savefig('plots/low/NDFD_Low_Temp_{}.png'.format(i), dpi=450, bbox_inches='tight')
 
 
