03f61d58ceb865ba03113a563ee43c9008567078
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-06-13 23:27:28 -0400

    Update ndfd_high_plan.py
    smoothing and isotherms

--- ndfd_high_plan.py
+++ ndfd_high_plan.py
@@ -6,6 +6,7 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import numpy as np
 from metpy.plots import USCOUNTIES
+from scipy.ndimage import gaussian_filter
 
 start_date = datetime.datetime.now()
 
@@ -34,15 +35,16 @@ if time_dim:
     for i in range(min(9, len(hi_temp[time_dim]))):
         datetime_str = np.datetime_as_string(hi_temp[time_dim].values[i], unit='D')
         temp_f = (hi_temp[i,:,:] - 273.15) * 9/5 + 32
+        temp_f_smoothed = gaussian_filter(temp_f, sigma=2)
         fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
-        ax.set_extent([-86, -82, 41, 45])
+        ax.set_extent([-87.5, -82, 41, 46.5])
         ax.add_feature(cfeature.STATES, linewidth=0.5)
         ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
-        plt.contourf(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, cmap='jet', levels=np.arange(30, 111, 1))
+        plt.contourf(ds_latlon['longitude'], ds_latlon['latitude'], temp_f_smoothed, cmap='jet', levels=np.arange(30, 111, 1))
         cbar = plt.colorbar(label='Temperature (F)', fraction=0.046, pad=0.04)
         cbar.set_ticks(np.arange(30, 111, 10))
         cbar.set_ticklabels(['30', '40', '50', '60', '70', '80', '90', '100', '110'])
-        contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f, colors='black', levels=np.arange(30, 111, 5))
+        contour = plt.contour(ds_latlon['longitude'], ds_latlon['latitude'], temp_f_smoothed, colors='black', levels=np.arange(30, 111, 5), linewidths=0.5)
         plt.clabel(contour, inline=True, fontsize=8, fmt='%1.0f')
         plt.title('NDFD High Temperature for {}'.format(datetime_str))
         plt.savefig('plots/high/NDFD_High_Temp_{}.png'.format(i), dpi=450, bbox_inches='tight')
